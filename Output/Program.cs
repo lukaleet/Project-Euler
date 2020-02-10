1fd34e59fdbdb90d500e9a6fd872e2e0749c5efd
lukaleet <lukamar495@student.polsl.pl>
2020-02-10 11:29:41 +0100

    Add Missing Commit With Solutions

--- Output/Program.cs
+++ Output/Program.cs
@@ -1,5 +1,6 @@
 ﻿using EulerTasks.Tasks.Even_Fibonacci_numbers;
 using EulerTasks.Tasks.Multiples_of_3_and_5;
+using Project_Euler.Euler_tasks.Largest_prime_factor;
 using Project_Euler.Euler_tasks.Multiplies_Of_3_And_5;
 using System;
 
@@ -12,6 +13,7 @@ namespace Output
             Console.WriteLine(Multiplies.FindSum(1000));
             Console.WriteLine(MultipliesLINQ.FindSum(1000));
             Console.WriteLine(Fibonacci.FindSum(4000000));
+            Console.WriteLine(Prime.FindLargestFactor(600851475143));
         }
     }
 }
--- Project Euler/Euler tasks/Largest prime factor/Prime.cs
+++ Project Euler/Euler tasks/Largest prime factor/Prime.cs
@@ -1,13 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Project_Euler.Euler_tasks.Largest_prime_factor
 {
     public static class Prime
     {
-        //public static int FindLargestFactor(int number)
-        //{
-        //}
+        public static int FindLargestFactor(long number)
+        {
+            int divider = 2;
+            List<int> numbers = new List<int>();
+            while(number != 1)
+            {
+                if (number % divider == 0)
+                {
+                    number /= divider;
+                    if (IsPrime(divider))
+                    {
+                        numbers.Add(divider);
+                    }
+                }
+                divider++;          
+            }
+
+            return numbers.Max();
+        }
+
+        public static bool IsPrime(int number)
+        {
+            int root = Convert.ToInt32(Math.Sqrt(number));
+            for (int i = 2; i <= root; i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+            return true;
+        }
     }
 }
